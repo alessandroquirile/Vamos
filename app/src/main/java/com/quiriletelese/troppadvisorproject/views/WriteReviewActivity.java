daf3b54b430201c86d2eef451d7fc3ec0fd2e695
alessandroquirile <a.quirile@studenti.unina.it>
2020-05-21 22:46:48 +0200

    Aggiunto WriteReviewActivity Verificarne la correttezza

---
+++ app/src/main/java/com/quiriletelese/troppadvisorproject/views/WriteReviewActivity.java
@@ -0,0 +1,18 @@
+package com.quiriletelese.troppadvisorproject.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.quiriletelese.troppadvisorproject.R;
+
+public class WriteReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review);
+
+        // TODO: inizializzare components e fare qualche prova
+    }
+}
