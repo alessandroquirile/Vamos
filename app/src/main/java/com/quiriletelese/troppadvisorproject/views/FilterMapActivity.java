7a9d6ad8f297ff412e3e4ddde0b439525de6bbe3
mauro <mau.telese@studenti.unina.it>
2020-07-28 12:00:23 +0200

    Aggiunte la resource relativa ai filtri su mappa

---
+++ app/src/main/java/com/quiriletelese/troppadvisorproject/views/FilterMapActivity.java
@@ -0,0 +1,16 @@
+package com.quiriletelese.troppadvisorproject.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.quiriletelese.troppadvisorproject.R;
+
+public class FilterMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_map);
+    }
+}
