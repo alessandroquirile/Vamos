7937afc72ab3f152651821c70246edafded7a765
mauro telese <mau.telese@studenti.unina.it>
2021-02-22 17:57:19 +0100

    added layout for leaderboard activity

---
+++ app/src/main/java/com/quiriletelese/troppadvisorproject/views/LeaderboardActivity.java
@@ -0,0 +1,16 @@
+package com.quiriletelese.troppadvisorproject.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.quiriletelese.troppadvisorproject.R;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
