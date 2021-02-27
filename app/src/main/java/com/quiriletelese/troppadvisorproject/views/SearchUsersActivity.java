1bcff4ab1e3335d01dc280da652194828036d35b
mauro telese <mau.telese@studenti.unina.it>
2021-02-27 19:47:43 +0100

    Added menu on search users activity

--- app/src/main/java/com/quiriletelese/troppadvisorproject/views/SearchUsersActivity.java
+++ app/src/main/java/com/quiriletelese/troppadvisorproject/views/SearchUsersActivity.java
@@ -1,8 +1,13 @@
 package com.quiriletelese.troppadvisorproject.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 
+import android.app.SearchManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.quiriletelese.troppadvisorproject.R;
 
@@ -14,4 +19,13 @@ public class SearchUsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_users);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_users_activity, menu);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search_users));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setIconifiedByDefault(false);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
 }
